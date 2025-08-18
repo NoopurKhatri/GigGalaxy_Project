ff5bb7b2fbc645228e7921a74427ec3fb48fbe96
Noopur Khatri <noopurkhatri41434@gmail.com>
2025-08-18 16:19:01 +0530

    Secure auth with jwt and cookies

--- api/server.js
+++ api/server.js
@@ -8,6 +8,7 @@ import conversationRoute from "./routes/conversation.route.js";
 import messageRoute from "./routes/message.route.js";
 import reviewRoute from "./routes/review.route.js";
 import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 
 const app = express();
@@ -24,6 +25,8 @@ const connect = async() =>{
 };
 
 app.use(express.json());
+app.use(cookieParser());
+
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
@@ -32,6 +35,13 @@ app.use("/api/orders", orderRoute);
 app.use("/api/conversations", conversationRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/reviews", reviewRoute);
+
+app.use((err,req,res,next)=>{
+    const errorStatus = err.status || 500;
+    const errMessage = err.message || "Something went wrong!";
+
+    return res.status(errorStatus).send(errMessage);
+});
 
 app.listen(8800,()=>{
     connect();
